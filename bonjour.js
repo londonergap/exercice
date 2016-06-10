777f96af10f0f3c13a2243b59afda9d681c0eccf
londonergap <fabienvalero@orange.fr>
2016-06-10 04:39:06 +0200

    ajout d'un module pour demander l'âge de l'utilisateur

--- bonjour.js
+++ bonjour.js
@@ -1,3 +1,6 @@
 var prenom = prompt("Veuillez entrez votre prénom");
 
 var nom = prompt("Bonjour " + prenom + "\nVeuillez entrer votre nom de famille.");
+
+var age = prompt(prenom + " " + nom + "\nQuel âge avez vous?");
+age = Number(age);
